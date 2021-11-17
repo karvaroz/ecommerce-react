d3c3ad64de033d55479930a54f6dc00af47c6169
karina <vargasriosk@gmail.com>
2021-11-17 16:27:13 -0500

    Subiendo imagenes

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,32 +1,29 @@
 
 import React from "react";
-import { Fade } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
-
-
-
-
-const fadeImages = [
-    "https://unsplash.com/photos/OF2YeWzvwJk",
-    "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-    "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-];
 
 function Slider() {
     return (
-        <div className="slide-container">
-            <Fade>
-                <div className="each-fade">
-                    <img src={fadeImages[0]} alt="slider" />
+        <>
+            <section className="home" id="home">
+                <div className="swiper-container home-slider">
+                    <div className="swiper-wrapper wrapper">
+                        <div className="swiper-slide slide">
+                            <div className="content">
+                                <span>our special dish</span>
+                                <h3>spicy noodles</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit natus dolor cumque?</p>
+                                <a href="#Home" className="btn">order now</a>
+                            </div>
+                            <div className="image">
+                                <img src="home-img-1.png" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="swiper-pagination"></div>
                 </div>
-                <div className="each-fade">
-                    <img src={fadeImages[1]} alt="slider" />
-                </div>
-                <div className="each-fade">
-                    <img src={fadeImages[2]} alt="slider" />
-                </div>
-            </Fade>
-        </div>
+            </section>
+        </>
     )
 }
+
 export default Slider;
